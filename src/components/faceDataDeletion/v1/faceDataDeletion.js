4a9b0b6ae4d53a133e79174b3baebf1e81423f0a
EinerZG <einer.zapata@moove-it.com>
2022-04-06 15:46:55 -0500

    change the verb to get

--- src/components/faceDataDeletion/v1/faceDataDeletion.js
+++ src/components/faceDataDeletion/v1/faceDataDeletion.js
@@ -2,11 +2,11 @@ const Router = require('koa-router');
 
 const router = new Router();
 
-router.post('/', async ctx => {
+router.get('/', async ctx => {
   try {
     console.log(ctx)
-    console.log(ctx.request)
-    console.log('-----------');
+    // console.log(ctx.request)
+    // console.log('-----------');
     ctx.body = 'OK';
   } catch (error) {
     console.log(error.message);

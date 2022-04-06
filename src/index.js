0882ab54c1cf8006bf7b166c6d19f79412761936
EinerZG <einer.zapata@moove-it.com>
2022-04-06 14:07:19 -0500

    Improve the server code

--- src/index.js
+++ src/index.js
@@ -6,11 +6,10 @@ const app = new Koa();
 const v1Router = new Router({ prefix: '/v1' });
 const PORT = 4000;
 
-// v1Router.use(async ctx => {
-//   ctx.body = 'Hello world';
-// });
+v1Router.use(async ctx => {
+  ctx.body = 'Hello world';
+});
 v1Router.use('/faceDataDeletion', faceDataDeletion.v1.routes());
 
 app.use(v1Router.routes());
-app.listen(PORT);
-console.log('Loading the in the port', PORT);
+app.listen(4000);
